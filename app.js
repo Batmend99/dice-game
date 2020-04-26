6d138bc93934852289ba1c4d26c6d048219ab55a
mede <glee9532@gmail.com>
2020-04-26 21:21:03 +0800

    Shoog shiddeh bolgoh

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 // Тооны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн тоог энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6);
 
 /* <div class="player-score" id="score-0">43</div> */
 // document.querySelector("#score-0").textContent = dice;
 // document.querySelector("#score-1").innerHTML = "<em>Hurry!<em>";
 
 //Програм эхлэхэд бэлтгэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-console.log("Шоо: " + dice) + 1;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
