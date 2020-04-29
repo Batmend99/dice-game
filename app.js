f4dc08da12f8c6b49513a0a20346ebdc18d446e2
mede <glee9532@gmail.com>
2020-04-29 13:49:49 +0800

    Toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 1;
+var activePlayer = 0;
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
@@ -17,8 +17,43 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+
+// Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 доторх санамсаргүй нэг тоог гаргаж авна.
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  // Шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
+
+  // Буусан санамсаргүй тоонд харгалзах шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
+
+  // Буусан тоо 1-ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ
+  if (diceNumber !== 1) {
+    // 1-ээс ялгаатай тоо буулаа.
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 буусан тул тоглогчийн ээлжийг солино.
+    // Хэрэв идэвхтэй тоглогч 0 байвал тус тоглогчийг 1 болгоно.
+    // Эсвэл идэвхтэй тоглогчийг 0 болгоно.
+
+    // Тухайн тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = "0";
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    // Улаан цэгийг шилжүүлэх
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // Шоог түр алга болгоно.
+    diceDom.style.display = "none";
+    // if (activePlayer === 0) {
+    //   activePlayer = 1;
+    // } else {
+    //   activePlayer = 0;
+    // }
+  }
 });
